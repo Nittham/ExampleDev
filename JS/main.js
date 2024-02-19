da80a2470a78621598b141009895ebf1b4a42940
Deerendra <deerendra.work@gmail.com>
2024-02-19 18:34:48 +0530

    Arrays and forloops

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,74 @@
-//datatypes
-const name='Deerendra'; //String
-const year=19; //number
-const rating=4.5; // is also just a number
-const boolean=true;
-const x=null; //null
-const y= undefined; //can assign undefined
-let z; // also undefined
+//arrays
+const arr = ['deer', 'lion', 10, null, true];
+console.log(arr);
+console.log(arr.length);
+console.log(arr.indexOf('lion'));
+arr.push('tiger');
+console.log(arr);
+arr.unshift('mole');
+console.log(arr);
+arr.pop();
+console.log(arr);
 
-console.log(typeof z);
+//objects
 
-//concatenation
+const person={
+    fname: 'Deerendra',
+    lname: 'Vasanthi',
+    age: 19,
+    hobbies: ['music', 'movies', 'sleeping'],
+    address: {
+        street: 'Mugil nagar',
+        city: 'Salem',
+        country: 'India'
+    }
+}
+console.log(person);
+const {fname, lname} = person;
+console.log(fname);
+const personJSON=JSON.stringify(person);
+console.log(personJSON);
 
-console.log('My name is '+ name + ' and I am '+ year);
+const todo=[
+    {
+        id: 1,
+        text: 'Get Laundry',
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        text: 'Eat snacks',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        text: 'Go for a walk',
+        isCompleted: true,
+    }
+];
 
-//template string
+//for loop
 
-console.log(`My name is ${name}`); //use backtick instead of quotations
+for(let i=0; i< todo.length; i++){
+    console.log(todo[i].text);
+}
+
+for(let task of todo){
+    console.log(task.text);
+}
+
+//forEach, map, filter
+
+todo.forEach(function(task) {
+    console.log(task.text);
+});
+
+const todoText = todo.map(function(task){
+    return task.text;
+});
+console.log(todoText);
+
+const todoCompleted=todo.filter(function(task){
+    return task.isCompleted===true;
+});
+console.log(todoCompleted);
