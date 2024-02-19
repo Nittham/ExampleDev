a989a4f739d8d2e0628ea51795987e6446c2dbe6
Deerendra <deerendra.work@gmail.com>
2024-02-19 16:40:39 +0530

    Variables

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+console.log('Hello world');
+console.error('This is an error');
+console.warn('This is a warning');
+
+//var, let, const --- variables; var not used;
+
+let age=19;
+console.log(age);
+age=20;
+console.log(age);
+const mark=90;
+mark=90; //causes error
+
