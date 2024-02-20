d7356ac4d2b6b23a69ff9b56383a57a8be06f688
Deerendra <deerendra.work@gmail.com>
2024-02-20 05:47:22 +0530

    Events

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    console.log('click');
+    document.querySelector('#my-form').style.background='#ccc';
+});
